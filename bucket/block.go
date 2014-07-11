aebe4ba62cff2114d5e64815baf13156fdfc4aba
Matt Coffin <mcoffin13@gmail.com>
2014-07-11 13:43:39 -0600

    Switch buckets to bytes.Buffer backing

--- bucket/block.go
+++ bucket/block.go
@@ -4,38 +4,43 @@ import (
     "bytes"
 )
 
-type bucket []byte
-
-func (self bucket) decoder(base int64) *BucketDecoder {
-    reader := bytes.NewBuffer(self)
-    dec := NewBucketDecoder(base, reader)
-    return dec
-}
-
 type Block struct {
-    buckets []bucket
+    buckets []*bytes.Buffer
     Baseline int64
 }
 
 func NewBlock(base int64, bucketCount int) *Block {
     block := &Block{
-        buckets: make([]bucket, bucketCount),
+        buckets: make([]*bytes.Buffer, bucketCount),
         Baseline: base,
+    }
+    for i, _ := range block.buckets {
+        block.buckets[i] = new(bytes.Buffer)
     }
     return block
 }
 
+func (self *Block) CreateBucketEncoder(index int) *BucketEncoder {
+    return NewBucketEncoder(self.Baseline, self.buckets[index])
+}
+
+func (self *Block) CreateBucketDecoder(index int) *BucketDecoder {
+    return NewBucketDecoder(self.Baseline, self.buckets[index])
+}
+
 func (self *Block) WriteBucket(index int, values []int64) {
-    buffer := &bytes.Buffer{}
-    enc := NewBucketEncoder(self.Baseline, buffer)
+    enc := self.CreateBucketEncoder(index)
     enc.Write(values)
     enc.Close()
-    self.buckets[index] = buffer.Bytes()
+}
+
+func (self *Block) Bucket(index int) []byte {
+    return self.buckets[index].Bytes()
 }
 
 func (self *Block) ReadBucket(index int, readBuffer []int64) []int64 {
     ret := make([]int64, 0)
-    dec := self.buckets[index].decoder(self.Baseline)
+    dec := self.CreateBucketDecoder(index)
     for {
         n, err := dec.Read(readBuffer)
         if n > 0 {
