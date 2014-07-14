e960396f69b26785edec8fa670b1c36de11b69b7
Matt Coffin <mcoffin13@gmail.com>
2014-07-14 15:07:12 -0600

    Change query api

---
+++ cqlcache.go
@@ -0,0 +1,60 @@
+package timedb
+
+import (
+    "errors"
+    "time"
+    "code.google.com/p/go-uuid/uuid"
+    "github.com/gocql/gocql"
+)
+
+type CQLCache struct {
+    Session *gocql.Session
+}
+
+func (self *CQLCache) Insert(entries chan Entry, series uuid.UUID, success chan error) {
+    for {
+        entry, more := <-entries
+        if more {
+            seriesUUID, err := gocql.UUIDFromBytes(series)
+            if err != nil {
+                success <- err
+                return
+            }
+            err = self.Session.Query("INSERT INTO cache (series, time, value) VALUES (?, ?, ?)", seriesUUID, entry.Timestamp, entry.Value).Exec()
+            if err != nil {
+                success <- err
+                return
+            }
+        } else {
+            success <- nil
+            return
+        }
+    }
+}
+
+func (self *CQLCache) Querier(granularity time.Duration, aggregator string) (Querier, error) {
+    if aggregator != "raw" {
+        return nil, errors.New("Attempt to find aggregator on cache")
+    }
+    return self, nil
+}
+
+func (self *CQLCache) Query(entries chan Entry, series uuid.UUID, start time.Time, end time.Time, success chan error) {
+    seriesUUID, err := gocql.UUIDFromBytes(series)
+    if err != nil {
+        success <- err
+        return
+    }
+    iter := self.Session.Query("SELECT time, value FROM cache WHERE series = ? AND time > ? AND time < ?", seriesUUID, start, end).Iter()
+    var timestamp time.Time
+    var value float64
+    for iter.Scan(&timestamp, &value) {
+        entries <- Entry{
+            Timestamp: timestamp,
+            Value: value,
+        }
+    }
+    close(entries)
+    err = iter.Close()
+    success <- err
+}
