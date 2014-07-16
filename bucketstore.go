40e7c211a24673ea52b81214bfab8669c09d5cce
Matt Coffin <mcoffin13@gmail.com>
2014-07-16 09:41:06 -0600

    Godoc comments for bucketstore

--- bucketstore.go
+++ bucketstore.go
@@ -4,12 +4,14 @@ import (
     "time"
 )
 
+// A representation of a backing store that keeps data in blocks and buckets
 type BucketStore struct {
     Duration time.Duration
     Granularities []time.Duration
     Aggregations []string
 }
 
+// Rounds a time based on the duration of this BucketStore
 func (self *BucketStore) nearestStart(t time.Time) time.Time {
     delta := t.Unix() % int64(self.Duration.Seconds())
     return time.Unix(t.Unix() - delta, 0)
