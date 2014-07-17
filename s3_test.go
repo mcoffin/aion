e5050931116a3d1eb2a70338b723e6e32991f1e8
Matt Coffin <mcoffin13@gmail.com>
2014-07-17 13:09:58 -0600

    Add query benchmarking

--- s3_test.go
+++ s3_test.go
@@ -28,6 +28,32 @@ func TestS3BucketStore(t *testing.T) {
     testQueryLevel(store, t)
 }
 
+func BenchmarkS3Query(b *testing.B) {
+    auth, err := aws.EnvAuth()
+    if err != nil {
+        b.Fatal(err)
+    }
+    myS3 := s3.New(auth, aws.USWest)
+    bucket := myS3.Bucket("timedb-dev")
+    store := &S3BucketStore{
+        BucketStore{
+            Duration: 60 * time.Second,
+            Granularity: 0,
+            Aggregations: []string{"min", "max", "avg"},
+            Multiplier: math.Pow10(1),
+        },
+        bucket,
+    }
+    store.Storer = store
+    b.ResetTimer()
+    for i := 0; i < b.N; i++ {
+        err = runQuery(store, testSeriesUUID, "avg", testStart, store.Duration)
+    }
+    if err != nil {
+        b.Fatal(err)
+    }
+}
+
 func BenchmarkS3Get(b *testing.B) {
     auth, err := aws.EnvAuth()
     if err != nil {
