64f4bad6f9bf9878704975bedd00638b9e0700fb
Matt Coffin <mcoffin13@gmail.com>
2014-07-22 15:28:14 -0600

    Switch to valueEncoder structs

--- bucketstore.go
+++ bucketstore.go
@@ -2,6 +2,7 @@ package timedb
 
 import (
     "bytes"
+    "io"
     "time"
     "code.google.com/p/go-uuid/uuid"
     "github.com/FlukeNetworks/timedb/aggregate"
@@ -29,16 +30,22 @@ func (self *aggregationContext) reset(start time.Time, duration time.Duration) {
     self.end = self.start.Add(duration)
 }
 
+type BucketStorer interface {
+    StoreBucket(store *BucketStore, times *bytes.Buffer, values []*bytes.Buffer, start time.Time, baseline float64, series uuid.UUID) error
+}
+
 type BucketStore struct {
     Duration time.Duration
     Granularity time.Duration
     Aggregators map[string]aggregate.Aggregator
     Multiplier float64
+    Storer BucketStorer
     aContext *aggregationContext
     buffers map[string]*bytes.Buffer
-    encoders map[string]*bucket.BucketEncoder
+    encoders map[string]*valueEncoder
     timeBuffer *bytes.Buffer
     timeEncoder *bucket.BucketEncoder
+    start time.Time
 }
 
 func (self *BucketStore) resetAggregators() {
@@ -47,14 +54,10 @@ func (self *BucketStore) resetAggregators() {
     }
 }
 
-func (self BucketStore) marshalFloat(v float64) int64 {
-    return int64(v * self.Multiplier)
-}
-
 func (self *BucketStore) flushAggregators() {
     self.timeEncoder.WriteInt(self.aContext.start.Unix())
     for name, enc := range self.encoders {
-        enc.WriteInt(self.marshalFloat(self.Aggregators[name].Value()))
+        enc.Write(self.Aggregators[name].Value())
     }
     self.aContext.reset(self.aContext.end, self.Granularity)
     self.resetAggregators()
@@ -63,25 +66,48 @@ func (self *BucketStore) flushAggregators() {
 func (self *BucketStore) Insert(series uuid.UUID, entry Entry, aggregator string) error {
     // First-time setup
     if self.aContext == nil {
-        start := entry.Timestamp.Truncate(self.Duration)
-        self.aContext = newAggregationContext(start, self.Granularity)
+        self.start = entry.Timestamp.Truncate(self.Duration)
+        self.aContext = newAggregationContext(self.start, self.Granularity)
         self.resetAggregators()
         if self.encoders == nil {
-            self.encoders = make(map[string]*bucket.BucketEncoder)
+            self.encoders = make(map[string]*valueEncoder)
         }
         self.timeBuffer = new(bytes.Buffer)
-        self.timeEncoder = bucket.NewBucketEncoder(start.Unix(), self.timeBuffer)
+        self.timeEncoder = bucket.NewBucketEncoder(self.start.Unix(), self.timeBuffer)
         for name, _ := range self.Aggregators {
             buf := new(bytes.Buffer)
             self.buffers[name] = buf
-            self.encoders[name] = bucket.NewBucketEncoder(self.marshalFloat(entry.Value), buf)
+            self.encoders[name] = newValueEncoder(entry.Value, self.Multiplier, buf)
         }
     } else {
         if self.aContext.needsRollup(entry.Timestamp) {
             self.flushAggregators()
         }
+        if entry.Timestamp.After(self.start.Add(self.Duration)) {
+        }
     }
     // Add the new value to the correct aggregator
     self.Aggregators[aggregator].Add(entry.Value)
     return nil
+}
+
+type valueEncoder struct {
+    enc *bucket.BucketEncoder
+    multiplier float64
+}
+
+func newValueEncoder(baseline float64, multiplier float64, writer io.Writer) *valueEncoder {
+    enc := &valueEncoder{
+        multiplier: multiplier,
+        enc: bucket.NewBucketEncoder(int64(baseline * multiplier), writer),
+    }
+    return enc
+}
+
+func (self *valueEncoder) Write(v float64) {
+    self.enc.WriteInt(int64(v * self.multiplier))
+}
+
+func (self *valueEncoder) Close() {
+    self.enc.Close()
 }
