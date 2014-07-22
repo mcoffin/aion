e04bb2603b7c3d1e164cb2b4611ab3f759124f10
Matt Coffin <mcoffin13@gmail.com>
2014-07-22 12:54:58 -0600

    New bucketstore implementation

--- bucketstore.go
+++ bucketstore.go
@@ -1,19 +1,85 @@
 package timedb
 
+import (
+    "bytes"
+    "time"
+    "github.com/FlukeNetworks/timedb/aggregate"
+    "github.com/FlukeNetworks/timedb/bucket"
+)
+
+type aggregationContext struct {
+    start, end time.Time
+}
+
+func newAggregationContext(start time.Time, duration time.Duration) *aggregationContext {
+    ac := &aggregationContext{
+        start: start.Truncate(duration),
+    }
+    ac.end = ac.start.Add(duration)
+    return ac
+}
+
+func (self aggregationContext) needsRollup(now time.Time) bool {
+    return now.After(self.end)
+}
+
+func (self *aggregationContext) reset(start time.Time, duration time.Duration) {
+    self.start = start.Truncate(duration)
+    self.end = self.start.Add(duration)
+}
+
 type BucketStore struct {
     Duration time.Duration
     Granularity time.Duration
-    Aggregations []string
+    Aggregators map[string]aggregate.Aggregator
     Multiplier float64
-    Storer BucketStorer
+    aContext *aggregationContext
+    buffers map[string]*bytes.Buffer
+    encoders map[string]*bucket.BucketEncoder
+    timeBuffer *bytes.Buffer
+    timeEncoder *bucket.BucketEncoder
 }
 
-func (self *BucketStore) Insert(entries chan Entry, errors chan error) {
-    for {
-        entry, more := <-entries
-        if !more {
-            return
-        }
-        start := entry.Timestamp.Truncate(self.Duration)
+func (self *BucketStore) resetAggregators() {
+    for _, a := range self.Aggregators {
+        a.Reset()
     }
+}
+
+func (self BucketStore) marshalFloat(v float64) int64 {
+    return int64(v * self.Multiplier)
+}
+
+func (self *BucketStore) flushAggregators() {
+    timeEncoder.WriteInt(self.aContext.start.Unix())
+    for name, enc := range encoders {
+        enc.WriteInt(self.marshalFloat(self.Aggregators[name].Value()))
+    }
+    self.aContext.reset(self.aContext.end)
+    self.resetAggregators()
+}
+
+func (self *BucketStore) Insert(series uuid.UUID, entry Entry, aggregator string) error {
+    // First-time setup
+    if self.aContext == nil {
+        start := entry.Timestamp.Truncate(self.Duration)
+        self.aContext = newAggregationContext(start, self.Granularity)
+        self.resetAggregators()
+        if self.encoders == nil {
+            self.encoders = make(map[string]*bucket.BucketEncoder)
+        }
+        self.timeBuffer = new(bytes.Buffer)
+        self.timeEncoder = bucket.NewBucketEncoder(start, self.timeBuffer)
+        for name, _ := range self.Aggregators {
+            self.buffers[name] = new(bytes.Buffer)
+            self.encoders[name] = bucket.NewBucketEncoder(self.marshalFloat(entry.Value), buf)
+        }
+    } else {
+        if self.aContext.needsRollup(entry.Timestamp) {
+            self.flushAggregators()
+        }
+    }
+    // Add the new value to the correct aggregator
+    self.Aggregators[aggregator].Add(entry.Value)
+    return nil
 }
--- timedb.go
+++ timedb.go
@@ -11,47 +11,21 @@ const (
 
 // One entry in a time series
 type Entry struct {
-    Series uuid.UUID
     Timestamp time.Time
     Value float64
 }
 
 type QueryLevel interface {
-    Insert(entries chan Entry, errors chan error)
+    Insert(series uuid.UUID, entry Entry, aggregator string) error
 }
 
 type TimeDB struct {
     QueryLevels []QueryLevel
-    entryChannels []chan Entry
-    errorChannels []chan error
 }
 
 func NewTimeDB(qLevels ...QueryLevel) *TimeDB {
     db := &TimeDB{
         QueryLevels: qLevels,
-        entryChannels: make([]chan Entry, len(qLevels),
-        errorChannels: make([]chan error, len(qLevels),
     }
     return db
-}
-
-func (self *TimeDB) Start() {
-    for i, level := range self.QueryLevels {
-        self.entryChannels[i] = make(chan Entry, channelSize)
-        self.errorChannels[i] = make(chan error)
-        go level.Insert(self.entryChannels[i], self.errorChannels[i])
-    }
-}
-
-func (self *TimeDB) Put(entry Entry) error {
-    for _, entries := range self.entryChannels {
-        entries <- entry
-    }
-    for _, errors := range self.errorChannels {
-        err := <-errors
-        if err != nil {
-            return err
-        }
-    }
-    return nil
 }
