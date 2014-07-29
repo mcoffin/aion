2b683caec795ca11c6b733976a4a1199e5c777d0
Matt Coffin <mcoffin13@gmail.com>
2014-07-29 17:32:57 -0600

    Add InMemoryBucketBulder skeleton

--- inmemory.go
+++ inmemory.go
@@ -2,10 +2,25 @@ package timedb
 
 import (
 	"code.google.com/p/go-uuid/uuid"
+	"bytes"
+	"github.com/FlukeNetworks/timedb/bucket"
 	"time"
 )
 
+type inMemoryBlockBuilderContext struct {
+	buffer bytes.Buffer
+	encoder *bucket.BucketEncoder
+}
+
+type inMemoryBucketBuilderContext struct {
+	end time.Time
+	contexts map[string]*inMemoryBlockBuilderContext
+}
+
 type InMemoryBucketBuilder struct {
+	Duration time.Duration
+	Multiplier float64
+	contexts map[string]map[time.Time]*inMemoryBucketBuilderContext
 }
 
 func (self *InMemoryBucketBuilder) Insert(series uuid.UUID, entry Entry) error {
