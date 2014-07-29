83b204305220c25df37426283fe084aa9e9608a6
Matt Coffin <mcoffin13@gmail.com>
2014-07-29 16:42:51 -0600

    Comment some confusing stuff in Query

--- bucketstore.go
+++ bucketstore.go
@@ -11,7 +11,6 @@ type BucketRepository interface {
 
 type BucketBuilder interface {
 	SeriesStore
-	WriteBuckets(repo BucketRepository) error
 }
 
 type BucketStore struct {
@@ -22,18 +21,22 @@ type BucketStore struct {
 }
 
 func (self *BucketStore) Query(series uuid.UUID, start, end time.Time, attributes []string) (EntryReader, error) {
+	// Create reader for the cache
 	builderReader, err := self.Builder.Query(series, start, end, attributes)
 	if err != nil {
 		return nil, err
 	}
+	// Create reader for the repository
 	repoReader, err := self.Builder.Query(series, start, end, attributes)
 	if err != nil {
 		return nil, err
 	}
 	readers := []EntryReader{builderReader, repoReader}
+	// start with the first reader (index = 0)
 	index := 0
 	var ret (func([]Entry) (int, error))
 	ret = func(entries []Entry) (int, error) {
+		// Read stuff from the current reader
 		n, err := readers[index].ReadEntries(entries)
 		// If we didn't get enough entries, try the next reader (if it exists)
 		if n < len(entries) {
@@ -46,6 +49,7 @@ func (self *BucketStore) Query(series uuid.UUID, start, end time.Time, attribute
 		}
 		return n, err
 	}
+	// Cast ret to type queryFunc for returning
 	return queryFunc(ret), nil
 }
 
