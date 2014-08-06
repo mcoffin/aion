f4c70a3458ff66ed26a36cb810a8db5cbaaefa6e
Matt Coffin <mcoffin13@gmail.com>
2014-08-06 16:17:08 -0600

    Add unit test for memory builder sourcing Unit tests for builder implementation of #6

--- memory_test.go
+++ memory_test.go
@@ -7,6 +7,52 @@ import (
 	"time"
 )
 
+type fakeQueryFunc func(start, end time.Time, entries chan Entry)
+
+func (self fakeQueryFunc) Query(series uuid.UUID, start, end time.Time, attributes []string, entries chan Entry, errors chan error) {
+	self(start, end, entries)
+}
+
+func TestMemoryCacheSourcing(t *testing.T) {
+	source := fakeQueryFunc(func(start, end time.Time, entries chan Entry) {
+		e := Entry{
+			Timestamp:  start,
+			Attributes: map[string]float64{"raw": 66.6},
+		}
+		entries <- e
+	})
+	store := MemoryBucketBuilder{
+		Duration:   60 * time.Second,
+		Multiplier: math.Pow10(1),
+		Source:     source,
+	}
+	store.Init()
+	filter := NewAggregateFilter(0, []string{"raw"}, nil)
+	level := Level{
+		Filter: filter,
+		Store:  &store,
+	}
+	level.Filter.SetHandler(level.Store.Insert)
+	entryTime := time.Now()
+	ent := Entry{
+		Timestamp:  entryTime,
+		Attributes: map[string]float64{"raw": 79.1},
+	}
+	series := uuid.NewRandom()
+	err := level.Filter.Insert(series, ent)
+	if err != nil {
+		t.Error(err)
+	}
+	bucketTime := entryTime.Truncate(store.Duration)
+	queryCount := 0
+	ForAllQuery(series, bucketTime, bucketTime.Add(store.Duration), []string{"raw"}, level.Store, func(entry Entry) {
+		queryCount++
+	})
+	if queryCount != 2 {
+		t.Errorf("Queried out %d items instead of 2\n", queryCount)
+	}
+}
+
 func TestMemoryCacheDoubleQuery(t *testing.T) {
 	store := MemoryBucketBuilder{
 		Duration:   60 * time.Second,
