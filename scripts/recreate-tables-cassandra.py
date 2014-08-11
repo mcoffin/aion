a2ed3dd9cbca26f8916966e34819ef95f4ec0723
Matt Coffin <mcoffin13@gmail.com>
2014-08-11 16:06:01 -0600

    Bugfix in cassandra scripts

--- scripts/recreate-tables-cassandra.py
+++ scripts/recreate-tables-cassandra.py
@@ -4,5 +4,13 @@ from cassandra.cluster import Cluster
 
 cluster = Cluster(['172.28.128.2'])
 session = cluster.connect('timedb')
-session.execute('DROP TABLE cache;')
+try:
+	session.execute('DROP TABLE cache;')
+except:
+	print('\'cache\' did not exist.')
 session.execute('CREATE TABLE cache (series uuid, time timestamp, value double, PRIMARY KEY (series, time));')
+try:
+	session.execute('DROP TABLE buckets;')
+except:
+	print('\'buckets\' did not exist.')
+session.execute('CREATE TABLE buckets (series uuid, time timestamp, attribs map<text, blob>, PRIMARY KEY (series, time));')
