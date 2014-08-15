3b1a55a7f1c2da25cded7d517b262c481f5413bc
Matt Coffin <mcoffin13@gmail.com>
2014-08-15 16:09:41 -0600

    Add unit test for BucketStore sourcing

--- bucketstore_test.go
+++ bucketstore_test.go
@@ -4,7 +4,53 @@ import (
 	"math"
 	"testing"
 	"time"
+
+	"code.google.com/p/go-uuid/uuid"
 )
+
+type fakeQueryFunc func(start, end time.Time, entries chan Entry)
+
+func (self fakeQueryFunc) Query(series uuid.UUID, start, end time.Time, attributes []string, entries chan Entry, errors chan error) {
+	self(start, end, entries)
+}
+
+func TestBucketStoreSourcing(t *testing.T) {
+	//	source := fakeQueryFunc(func(start, end time.Time, entries chan Entry) {
+	//		e := Entry{
+	//			Timestamp:  start,
+	//			Attributes: map[string]float64{"raw": 66.6},
+	//		}
+	//		entries <- e
+	//	})
+	store := NewBucketStore(60*time.Second, math.Pow10(1))
+	filter := NewAggregateFilter(0, []string{"raw"}, nil)
+	level := Level{
+		Filter: filter,
+		Store:  store,
+	}
+	level.Filter.SetHandler(level.Store.Insert)
+	entryTime := time.Now()
+	ent := Entry{
+		Timestamp:  entryTime,
+		Attributes: map[string]float64{"raw": 79.1},
+	}
+	series := uuid.NewRandom()
+	err := level.Filter.Insert(series, ent)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bucketTime := entryTime.Truncate(store.Duration)
+	queryCount := 0
+	err = ForAllQuery(series, bucketTime, bucketTime.Add(store.Duration), []string{"raw"}, level.Store, func(entry Entry) {
+		queryCount++
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	if queryCount != 2 {
+		t.Errorf("Queried out %d items instead of 2!\n", queryCount)
+	}
+}
 
 func TestBucketStore(t *testing.T) {
 	store := NewBucketStore(60*time.Second, math.Pow10(1))
