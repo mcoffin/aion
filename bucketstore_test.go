e2a0a21c8cec9ada845a2f44c5e0ba2e0755c90d
Matt Coffin <mcoffin13@gmail.com>
2014-08-15 16:41:46 -0600

    Add multiquery unit test

--- bucketstore_test.go
+++ bucketstore_test.go
@@ -14,6 +14,38 @@ func (self fakeQueryFunc) Query(series uuid.UUID, start, end time.Time, attribut
 	self(start, end, entries)
 }
 
+func TestBucketStoreMultiQuery(t *testing.T) {
+	store := NewBucketStore(60*time.Second, math.Pow10(1))
+	filter := NewAggregateFilter(0, []string{"raw"}, nil)
+	level := Level{
+		Filter: filter,
+		Store:  store,
+	}
+	level.Filter.SetHandler(level.Store.Insert)
+	entryTime := time.Now()
+	ent := Entry{
+		Timestamp:  entryTime,
+		Attributes: map[string]float64{"raw": 79.1},
+	}
+	series := uuid.NewRandom()
+	err := level.Filter.Insert(series, ent)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i := 0; i < 2; i++ {
+		queryCount := 0
+		err = ForAllQuery(series, entryTime, entryTime, []string{"raw"}, level.Store, func(entry Entry) {
+			queryCount++
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		if queryCount != 1 {
+			t.Errorf("Test %d yielded %d results instead of 1\n", i, queryCount)
+		}
+	}
+}
+
 func TestBucketStoreSourcing(t *testing.T) {
 	//	source := fakeQueryFunc(func(start, end time.Time, entries chan Entry) {
 	//		e := Entry{
