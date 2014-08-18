59a55662c4482fc4b1bd2caaa2e55eedee15abfb
Matt Coffin <mcoffin13@gmail.com>
2014-08-18 12:59:33 -0600

    Create dynamodb implementation

---
+++ dynamodb/dynamodb_test.go
@@ -0,0 +1,57 @@
+package dynamodb_test
+
+import (
+	"math"
+	"testing"
+	"time"
+
+	"github.com/FlukeNetworks/aion"
+	"github.com/FlukeNetworks/aion/aiontest"
+	aiondynamodb "github.com/FlukeNetworks/aion/dynamodb"
+	"github.com/crowdmob/goamz/aws"
+	"github.com/crowdmob/goamz/dynamodb"
+)
+
+func createDynamoDBTestServer() (*dynamodb.Server, error) {
+	auth, err := aws.EnvAuth()
+	if err != nil {
+		return nil, err
+	}
+	server := dynamodb.Server{
+		Auth:   auth,
+		Region: aws.Region{Name: "us-west-1", DynamoDBEndpoint: "http://localhost:8000"},
+	}
+	return &server, nil
+}
+
+func TestDynamoDBStore(t *testing.T) {
+	server, err := createDynamoDBTestServer()
+	if err != nil {
+		t.Fatal(err)
+	}
+	pk := dynamodb.PrimaryKey{
+		KeyAttribute: &dynamodb.Attribute{
+			Name: "series",
+			Type: dynamodb.TYPE_STRING,
+		},
+		RangeAttribute: &dynamodb.Attribute{
+			Name: "time",
+			Type: dynamodb.TYPE_NUMBER,
+		},
+	}
+	tbl := dynamodb.Table{
+		Server: server,
+		Name:   "timedb-bucket",
+		Key:    pk,
+	}
+	store := aion.NewBucketStore(60*time.Second, math.Pow10(1))
+	repo := aiondynamodb.Repository{
+		Table: &tbl,
+	}
+	store.Repository = repo
+	level := aion.Level{
+		Filter: aion.NewAggregateFilter(0, []string{"raw"}, nil),
+		Store:  store,
+	}
+	aiontest.TestLevel(&level, t, time.Second, store.Duration)
+}
