42071d5fc84fcc5e5d774aad77f312083f53000a
Matt Coffin <mcoffin13@gmail.com>
2016-01-06 11:22:31 -0700

    Add schema resource

---
+++ src/main/scala/com/netscout/aion2/resources/Schema.scala
@@ -0,0 +1,44 @@
+package com.netscout.aion2.resources
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.inject.Inject
+import com.netscout.aion2.model.AionObjectConfig
+
+import javax.ws.rs._
+import javax.ws.rs.core.{Response, StreamingOutput}
+import javax.ws.rs.core.MediaType._
+
+import scala.collection.JavaConversions._
+
+@Path("/schema")
+class Schema @Inject() (
+  val mapper: ObjectMapper
+) {
+  val schema: scala.collection.mutable.Set[AionObjectConfig] = scala.collection.mutable.Set.empty
+
+  /**
+   * Registers a new schema with this resource
+   */
+  def registerSchema(newSchema: Set[AionObjectConfig]) {
+    schema ++= newSchema
+  }
+
+  private def fieldsSchema = schema.map(obj => {
+    obj.indices.map(index => {
+      (index.name, obj.fields)
+    })
+  }).reduce(_++_).toMap
+
+  @GET
+  @Produces(Array(APPLICATION_JSON))
+  def getSchema = {
+    val stream = new StreamingOutput {
+      import java.io.OutputStream
+
+      override def write(output: OutputStream) {
+        mapper.writeValue(output, fieldsSchema)
+      }
+    }
+    Response.ok(stream).build()
+  }
+}
