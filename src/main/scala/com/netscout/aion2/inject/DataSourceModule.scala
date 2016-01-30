80e67668735320d25cb4d5a31864b9ce46995cb5
Matt Coffin <mcoffin13@gmail.com>
2016-01-29 17:46:14 -0700

    cassandra: allow port configuration

--- src/main/scala/com/netscout/aion2/inject/DataSourceModule.scala
+++ src/main/scala/com/netscout/aion2/inject/DataSourceModule.scala
@@ -9,12 +9,14 @@ import com.netscout.aion2.source.CassandraDataSource
 import net.codingwell.scalaguice.ScalaModule
 
 class CassandraSessionProvider @Inject() (
-  @TypesafeConfig("com.netscout.aion2.cassandra.contactPoints") contactPoints: java.util.List[String]
+  @TypesafeConfig("com.netscout.aion2.cassandra.contactPoints") contactPoints: java.util.List[String],
+  @TypesafeConfig("com.netscout.aion2.cassandra.port") cassandraPort: Integer
 ) extends Provider[Session] {
   import scala.collection.JavaConversions._
 
   val cluster = Cluster.builder()
     .addContactPoints(contactPoints : _*)
+    .withPort(cassandraPort)
     .build()
 
   lazy val session = cluster.connect()
